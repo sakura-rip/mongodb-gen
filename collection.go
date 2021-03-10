3548cc4b509331e109e9b80e5f932cffcbab47f5
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-11 01:15:53 +0900

    :heavy_plus_sign: add: comment

--- collection.go
+++ collection.go
@@ -1,17 +1,38 @@
 package main
 
 type Collection struct {
-	Name      string
+	//Structの名前
+	Name string
+	//strings.ToLower(Name)
 	LowerName string
-	Fields    []CollectionField
-
+	//Structの各フィールドの情報
+	//map[Bson名]CollectionField
+	Fields map[string]CollectionField
+	//パッケージ名
 	PackageName string
+	//対象Structの存在するファイルの名前
+	FileName string
+}
+
+func NewCollection() Collection {
+	return Collection{
+		Fields: map[string]CollectionField{},
+	}
 }
 
 type CollectionField struct {
-	RootName  string
+	//親の名前(Collection.Name)
+	RootName string
+	//フィールド名
 	FieldName string
+	//フィールドのType
 	FieldType string
+	//strings.ToLower(FieldName)
 	LowerName string
-	BsonName  string
+	//`bson:"BsonName"`
+	BsonName string
+}
+
+func NewCollectionField() CollectionField {
+	return CollectionField{}
 }
