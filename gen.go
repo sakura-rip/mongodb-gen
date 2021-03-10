b363739038c01c0e7a53e24b971d053c5f1eb2db
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-11 01:37:52 +0900

    :heavy_plus_sign: add: packageName args

--- gen.go
+++ gen.go
@@ -1,10 +1,11 @@
 package main
 
-func generateClientFile(collections []Collection) error {
+func generateClientFile(packageName string, collections []Collection) error {
 	return createTemplateFile(
 		GenDirName+"/client.go", "./templates/client.go.tmpl",
-		map[string][]Collection{
+		map[string]interface{}{
 			"Collections": collections,
+			"PackageName": packageName,
 		}, nil,
 	)
 }
