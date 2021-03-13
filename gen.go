2315ab293573afe71ef379746a77df7c94be5295
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-14 01:08:32 +0900

    :+1: change: use gen target dir

--- gen.go
+++ gen.go
@@ -1,18 +1,17 @@
 package main
 
-func generateClientFile(packageName string, collections []Collection) error {
+func generateClientFile(collections []Collection) error {
 	return createTemplateFile(
-		targetDirName+"/client.go", "./templates/client.go.tmpl",
+		genTargetDir+"/client.go", "./templates/client.go.tmpl",
 		map[string]interface{}{
 			"Collections": collections,
-			"PackageName": packageName,
 		}, nil,
 	)
 }
 
 func generateCollectionBaseFile(col Collection) error {
 	return createTemplateFile(
-		targetDirName+"/"+col.Name+"Base.go",
+		genTargetDir+"/"+col.Name+"Base.go",
 		"./templates/CollectionBase.go.tmpl",
 		col, nil,
 	)
