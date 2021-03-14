141abf4a70927eebf4ceab7c1e467d52fb0e4ac5
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-14 19:59:38 +0900

    :heavy_plus_sign: add: default field handler

---
+++ templates/collectionField_default.go
@@ -0,0 +1,35 @@
+package templates
+
+const (
+	DefaultField = `
+//#############################
+//[[.FieldName]] handlers
+//Type: [[.FieldType]]
+//#############################
+
+
+func (cl *[[.RootName]]DaoClient) Update[[.RootName]][[.FieldName]](id, [[.FieldName]] [[.FieldType]]) error {
+	return cl.UpdateAttribute(id, bson.D{{"[[.BsonName]]", [[.FieldName]]}})
+}
+
+func (cl *[[.RootName]]DaoClient) Delete[[.RootName]][[.FieldName]](id string) error {
+	return cl.DeleteAttributes(id, "[[.BsonName]]")
+}
+
+func (cl *[[.RootName]]DaoClient) Get[[.RootName]][[.FieldName]](id string) ([[.FieldType]], error) {
+	[[.LowerName]], err := cl.GetAttributes(id, "[[.BsonName]]")
+	if err != nil {
+		return "", err
+	}
+	return [[.LowerName]].Name, err
+}
+
+func (cl [[.RootName]]DaoClient) Get[[.RootName]]sBy[[.FieldName]]([[.LowerName]] [[.FieldType]]) ([][[.PackageName]].[[.RootName]], error) {
+	[[.LowerName]]s, err := cl.Get(bson.M{"[[.BsonName]]": [[.LowerName]]})
+	if err != nil {
+		return nil, err
+	}
+	return [[.LowerName]]s, nil
+}
+`
+)
