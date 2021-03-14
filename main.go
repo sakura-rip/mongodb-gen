8e626897114623458ded11d90b6f9a277c392023
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-03-14 14:29:25 +0900

    :heavy_plus_sign: add: dir check

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var (
@@ -16,8 +17,14 @@ func main() {
 	if len(os.Args) < 0 {
 		log.Fatal("target dir required")
 	}
-	targetDirName = os.Args[len(os.Args)-1]
-
+	targetDirName, err := filepath.Abs(os.Args[len(os.Args)-1])
+	if err != nil {
+		log.Fatal("error occurred during parsing directory: ", err)
+	}
+	fInfo, _ := os.Stat(targetDirName)
+	if !fInfo.IsDir() {
+		log.Fatal("error: ", targetDirName, "is not directory")
+	}
 	genTargetDir = targetDirName + "_dao"
 
 	cols := getAllCollections()
